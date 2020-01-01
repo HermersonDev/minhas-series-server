1166474a7188eeca74b75f3e5c0eabfbaf0da24c
HermersonDev <ne.hermerson@gmail.com>
2020-01-01 20:44:43 -0300

    Add CORS methods

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,9 @@ router.get('/', (req, res) => res.send({
 const api = require('express').Router()
 
 api.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header('Access-Control-Allow-Origin', '*'');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
 	next();
 })
 api.get('/', (req, res) => res.send({
